41d0e6858a771c58389101908caf01dcfa81f2f5
amirhnajafiz <najafizadeh21@gmail.com>
2023-12-23 17:12:08 +0330

    feat: first bot

--- bot.py
+++ bot.py
@@ -1,4 +1,13 @@
-data = {}
+import json
+import random
+
+
+BATCH = 2
+LIMIT = 5
+ALPHA = 2
+
+data = []
+export = []
 
 
 if __name__ == "__main__":
@@ -11,8 +20,25 @@ if __name__ == "__main__":
             
             print(f"[02] loading {parts[0]} batch.")
             
-            data[parts[0]] = [x.strip("\"") for x in parts[1].split(";")]
+            data.append({
+                "key": parts[0],
+                "list": [x.strip("\"") for x in parts[1].split(";")]
+            })
     
     print("[03] data loaded.")
+    
+    for _ in range(0, BATCH):
+        index = random.randint(0, len(data) - 2)
+        
+        item = data[index]['key']
+        array = random.sample(data[index]['list'], k=random.randint(1,LIMIT))
+        
+        if random.randint(0, 10) < ALPHA:
+            array += random.sample(data[len(data)-1], k=ALPHA)
+        
+        export.append({
+            "attack": item,
+            "vulenrabilities": array
+        })
 
-    print(data)
+    print(json.dumps(export))
